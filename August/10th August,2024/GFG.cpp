31fbcc4155b0acc4b7c54beae1861450a7f55454
sreecharan1306 <mscharan1313@gmail.com>
2024-08-10 11:19:21 +0530

    10th August,2024

---
+++ August/10th August,2024/GFG.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to rotate a linked list.
+    Node* rotate(Node* head, int k) {
+        // Your code here
+        Node* temp=head;
+        Node* prev=NULL;
+        int cnt=1;
+        while(temp->next){
+            if(cnt==k){
+                prev=temp;
+            }
+            cnt+=1;
+            temp=temp->next;
+            // if(!temp->next) return head;
+        }
+        if(cnt==k) return head;
+        // Node* end=temp;
+        // while(end){
+        //     if(end->next)
+        //         end=end->next;
+        //     else break;
+        // }
+        temp->next=head;
+        head=prev->next;
+        prev->next=NULL;
+        // end->next=head;
+        return head;
+        
+        
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.rotate(head, k);
+        printList(head);
+    }
+    return 1;
+}
+
+// } Driver Code Ends
